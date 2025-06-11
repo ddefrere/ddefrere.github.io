cb80a0cd9a02bbc1554acc447ad5f1442c922f34
ddefrere <denis.defrere@kuleuven.be>
2025-06-11 19:39:26 +0000

    Deploying to gh-pages from @ ddefrere/ddefrere.github.io@892850056f523ff6a1c22848f91aad347475f677 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -89,31 +89,11 @@ ninja.data = [{
           description: "Opticon-Radionet Pilot project (2021-2025).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-opticon-2016",
+          title: 'Opticon 2016',
+          description: "Design study and technology roadmap for the NOTT (formerly Hi-5) interferometer",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
             },},{
         id: 'social-email',
         title: 'email',
